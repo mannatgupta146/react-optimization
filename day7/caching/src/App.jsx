70e43cb4061660a3150a02d6c4eeed270ae916e5
Mannat Gupta <mannatgupta146@gmail.com>
2026-08-03 22:38:19 +0530

    day7

--- day7/caching/src/App.jsx
+++ day7/caching/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { fetchUsers } from "./api/api";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
 
-export default App
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>{error.message}</h1>;
+
+  return (
+    <div>
+      {data.map((user) => (
+        <h1 key={user.id}>{user.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default App;
