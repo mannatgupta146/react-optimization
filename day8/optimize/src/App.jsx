88abe2d7caf9410adc6f2c9a498f321ebba3afa4
Mannat Gupta <mannatgupta146@gmail.com>
2026-08-06 00:14:19 +0530

    optimize img

--- day8/optimize/src/App.jsx
+++ day8/optimize/src/App.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import { fetchUsers } from "./api/api";
 
 const App = () => {
@@ -8,17 +7,55 @@ const App = () => {
     queryFn: fetchUsers,
   });
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>{error.message}</h1>;
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-xl font-semibold">Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-red-500">{error.message}</h1>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      {data.map((user) => (
-        <div key={user.id}>
-          <h1>{user.title}</h1>
-          <img src={user.image} alt="" />
-        </div>  
-      ))}
+    <div className="min-h-screen bg-gray-100 py-10">
+      <div className="mx-auto w-[90%] max-w-7xl">
+        <h1 className="mb-8 text-3xl font-bold text-gray-800">
+          Products
+        </h1>
+
+        <div className="flex flex-wrap justify-center gap-8">
+          {data.map((user) => (
+            <div
+              key={user.id}
+              className="w-95 rounded-xl bg-white p-6 shadow-md transition duration-300 hover:shadow-lg"
+            >
+              <div className="flex h-64 items-center justify-center">
+                <img
+                  loading="lazy"
+                  src={user.image}
+                  alt={user.title}
+                  className="h-52 object-contain"
+                />
+              </div>
+
+              <h2 className="mt-5 text-xl font-semibold text-gray-800 line-clamp-2">
+                {user.title}
+              </h2>
+
+              <p className="mt-3 text-gray-600 line-clamp-4">
+                {user.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
